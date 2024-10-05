aeeafc91c2b3fa552054e9b49d400d0f17a0fb9f
Prince Isaac Pantino <arispantino@gmail.com>
2024-10-05 13:11:56 +0800

    docs: added better documentation

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,16 +1,24 @@
 const jwt = require('jsonwebtoken');
 
+// authentication w/ jwt
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers['authorization'];
+
+  // if no auth header, returns 401
   if (!authHeader) {
     return res.status(401).json({ message: 'No token provided' });
   }
 
+  // extracts token from auth header 
   const token = authHeader.split(' ')[1];
   jwt.verify(token, process.env.JWT_SECRET || 'secretkey', (err, decoded) => {
+
+    // returns 403 if token is invalid
     if (err) {
       return res.status(403).json({ message: 'Failed to authenticate token' });
     }
+
+    // sets req.userId to the decoded token's userId
     req.userId = decoded.userId;
     next();
   });
