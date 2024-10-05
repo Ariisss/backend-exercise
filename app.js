eb26edd2f7a0c90cf14b3052e330ec3e73ca4138
Prince Isaac Pantino <arispantino@gmail.com>
2024-10-05 14:26:30 +0800

    refactor: added proper spacing

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/user');
-const loggingMiddleware = require('./middleware/loggingMiddleware');const rateLimiter = require('./middleware/rateLimiterMiddleware');
+const loggingMiddleware = require('./middleware/loggingMiddleware');
+const rateLimiter = require('./middleware/rateLimiterMiddleware');
 
 const app = express();
 const PORT = 3000;
